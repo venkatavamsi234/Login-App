3c2cb4dc2ade4563eff260c04a42f4fc6e5722b3
User <user@full235-pro.full.creative>
2017-09-11 15:01:49 +0530

    Initial Commit

---
+++ Login AppTests/Login_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Login_AppTests.swift
+//  Login AppTests
+//
+//  Created by User on 11/09/17.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import XCTest
+@testable import Login_App
+
+class Login_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
